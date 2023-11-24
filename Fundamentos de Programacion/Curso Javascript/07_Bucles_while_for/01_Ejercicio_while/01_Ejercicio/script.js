8d96e713d5bcaa48f76f06e979fece6b06e22873
Kimberlly888 <kivargas.iccp@gmail.com>
2023-11-24 12:30:47 -0300

    ok

---
+++ Fundamentos de Programacion/Curso Javascript/07_Bucles_while_for/01_Ejercicio_while/01_Ejercicio/script.js
@@ -0,0 +1,12 @@
+// Desarrollo Ejercicio
+function contar (){
+    let contador = 20;
+    let array = [];
+
+    while(contador >= 10){
+        array.push(contador);
+        contador --;
+    }
+    alert(`Lista: ${array}`); 
+
+}
